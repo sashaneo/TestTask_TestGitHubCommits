07b3a0f89aee8f942d12689672c7c159b7f53b2b
Alexander Niedielkin <sashaneo011@gmail.com>
2020-02-17 15:48:34 +0200

    error on win

---
+++ backup_ver1.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+source = ['C:\\folder']
+target_dir = ['D:\\target']
+
+target = str(target_dir) + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+print((target))
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+
+if os.system(zip_command) == 0:
+    print('Резервная копия успешно создана в', target)
+else:
+    print('Создание резервной копии не удалось')
