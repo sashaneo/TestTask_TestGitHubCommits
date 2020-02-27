dcf3c289b2ec368f4c510972a58927796b206442
Alexander Niedielkin <sashaneo011@gmail.com>
2020-02-27 23:46:21 +0200

    comments

--- last_commit_unittest.py
+++ last_commit_unittest.py
@@ -11,9 +11,11 @@ class TestLastCommit(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
 
-    def test_get_last_commit_id(self):
-        '''Finding and assertion Ids'''
-        """getting commit id"""
+    def test_get_commit_id(self):
+        """Finding and assertion Ids"""
+
+        '''Getting id at commit page'''
+
         self.driver.get(comm_page)
         last_comm = self.driver.find_elements_by_xpath('//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/ol[*]/li[*]/div[2]/div/clipboard-copy')
         value_list = []
@@ -21,7 +23,7 @@ class TestLastCommit(unittest.TestCase):
             commit_value = val.get_attribute('value')
             value_list.append(commit_value)
 
-        """getting commit time"""
+        '''Getting commit time'''
 
         last_com_time = self.driver.find_elements_by_xpath('//*[@id="js-repo-pjax-container"]/div[2]/div/div[2]/ol[*]/li/div[*]/div/div[*]/relative-time')
         time_list = []
@@ -30,28 +32,29 @@ class TestLastCommit(unittest.TestCase):
             commit_time = datetime.strptime(dt, '%Y-%m-%dT%H:%M:%SZ')
             time_list.append(commit_time)
 
-
-        """creating a dictionary id/time"""
+        '''Creating a dictionary id/time'''
 
         dict_id_time = dict(zip(value_list, time_list))
         for k, v in dict_id_time.items():
             print(k, v)
 
-        """finding max value in dict"""
+        '''Finding max value in dict'''
 
         max_value = max(dict_id_time.values())
         final_dict = {k: v for k, v in dict_id_time.items() if v == max_value}
         id_comm_page = list(final_dict)[0]
         print('comm page: {}'.format(id_comm_page))
 
-        """getting id commit in the header"""
+        '''Getting id commit in the header of main page'''
+
         self.driver.get(main_page)
         element_repo = self.driver.find_element_by_xpath('//*[@id="js-repo-pjax-container"]/div[2]/div/div[6]/div/div/div[3]/a')
         link_repo = element_repo.get_attribute('href').split('/')
         id_main_page = link_repo[-1]
         print('main page: {}'.format(id_main_page))
 
-        """assertion commit id from different pages"""
+        '''Assertion commit ids from different pages'''
+
         assert id_main_page == id_comm_page
 
     def tearDown(self):
