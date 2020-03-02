01ec8000f2cfbd1dc997c56f66abade01be923ad
Alexander Niedielkin <sashaneo011@gmail.com>
2020-03-02 13:59:11 +0200

    added ver2

---
+++ last_com_ver2.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import unittest
+
+comm_page = 'https://github.com/django/django/commits/master'
+main_page = 'https://github.com/django/django'
+commit_value_path = '//clipboard-copy'
+element_repo_path = '//*[@class="no-wrap d-flex flex-self-start flex-items-baseline"]/a'
+
+
+class TestLastCommit(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+
+    def test_get_commit_id(self):
+        """Finding and assertion Ids"""
+
+        '''Getting id at commit page'''
+
+        self.driver.get(comm_page)
+        commit_value = self.driver.find_element_by_xpath(commit_value_path)
+        id_comm_page = commit_value.get_attribute('value')
+        print('comm page: {}'.format(id_comm_page))
+
+        '''Getting id commit in the header of main page'''
+
+        self.driver.get(main_page)
+        element_repo = self.driver.find_element_by_xpath(element_repo_path)
+        link_repo = element_repo.get_attribute('href').split('/')
+        id_main_page = link_repo[-1]
+        print('main page: {}'.format(id_main_page))
+
+        '''Assertion commit ids from different pages'''
+
+        assert id_main_page == id_comm_page
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
